79c91c03259c33050b4ba42ab4d23cf54cf5b1c8
QuUteO <quuteo86@gmail.com>
2026-03-02 14:44:02 +0300

    fix bug

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
     def __init__(self):
-        self.GEMINI_API_KEY = os.getenv("API_KEY")
+        self.GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+        self.BACKEND_URL = os.getenv("BACKEND_URL")
 
 config_obj = Config()
