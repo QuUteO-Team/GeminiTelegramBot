d756c71f911d7a32ac82c4882852a66a25b92360
QuUteO <quuteo86@gmail.com>
2026-03-03 15:39:52 +0300

    change: api.py file

--- api.py
+++ api.py
@@ -1,13 +1,17 @@
-from fastapi import FastAPI
 from gemini_client import ai_generate
+from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
+
+class PromptRequest(BaseModel):
+    prompt: str
+
 @app.post("/request")
-def send_prompt(
-        prompt: str
-):
-    answer = ai_generate.generate(prompt)
+
+def send_prompt(request: PromptRequest):
+    answer = ai_generate.generate(request.prompt)
 
     return {"answer": answer}
-
